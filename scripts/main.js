512a9a6ce9053e1101d7503104d051c7c9dbb614
Sam Mitchell <samuel.i.mitchell@gmail.com>
2023-09-15 16:24:56 -0400

    used JavaScript to populate drop down menus, added background image

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const planetArrayFormEl = document.querySelector('#planetArrayForm');
+const speciesArrayFormEl = document.querySelector('#speciesArrayForm');
 
 document.addEventListener('DOMContentLoaded', function () {
     console.log('DOM IS READY');
@@ -13,41 +14,68 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(function (response) {
             console.log('Planet Results: ', response);
             showPlanet(response.results[1].name, planet);
+            makePlanetList(response.results.map(x => {
+                console.log(x.name);
+                return(x.name);
+            }));
+            // console.log(response.results);
         })
     const speciesUrl = "https://swapi.dev/api/species/";
     get(speciesUrl)
         .then(function (response) {
             console.log('Species Results: ', response);
             showSpecies(response.results[2].name, species);
+            makeSpeciesList(response.results.map(y => {
+                console.log(y.name);
+                return(y.name);
+            }));
         })
-});
 
-function showPlanet(info, element) {
-    element.innerText = info;
-}
 
-function showSpecies(info, element) {
-    element.innerText = info;
-}
+    function showPlanet(info, element) {
+        element.innerText = info;
+    }
 
-function makePlanetList(planetArray) {
-    console.log(planetArray);
-    const selectEl = document.createElement('select');
-    planetArray.map(function (any) {
-        // create an option element
-        const option = document.createElement('option');
-        // define option attributes
-        option.value = any;
-        option.text = any;
-        // append the option to the <select>
-        selectEl.appendChild(option);
+
+    function showSpecies(info, element) {
+        element.innerText = info;
+    }
+
+    function makePlanetList(planetArray) {
+        console.log(planetArray);
+        const selectEl = document.getElementById('planetSelect');
+        planetArray.map(function (any) {
+            // create an option element
+            const option = document.createElement('option');
+            // define option attributes
+            option.value = any;
+            option.text = any;
+            // append the option to the <select>
+            selectEl.appendChild(option);
+        });
+        // append the <select> to the <form>
+        planetArrayFormEl.append(selectEl);
+    }
+
+    function makeSpeciesList(speciesArray) {
+        console.log(speciesArray);
+        const selectEl = document.getElementById('speciesSelect');
+        speciesArray.map(function (any) {
+            //create an option element
+            const option = document.createElement('option');
+            //define option attributes
+            option.value = any;
+            option.text = any;
+            // append the option to the <select>
+            selectEl.appendChild(option);
+        });
+        //append the <select> to the <form>
+        speciesArrayFormEl.append(selectEl);
+    }
+
+    planetArrayFormEl.addEventListener('submit', {
     });
-    // append the <select> to the <form>
-    planetArrayFormEl.append(selectEl);
-}
 
-planetArrayFormEl.addEventListener('submit', {
-
+    speciesArrayFormEl.addEventListener('submit', {
+    });
 });
-
-// I CAN'T SUCCESSFULLY CALL THE FUNCTION MAKEPLANETLIST !!!!!
